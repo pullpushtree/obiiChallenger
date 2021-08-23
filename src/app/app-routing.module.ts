1098cc225d4159f20d2dc350c92def1999c32775
pullpushtree <bad7legend@gmail.com>
2021-08-23 15:22:07 -0400

    Deleted Pilot Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+const routes: Routes = [ 
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '', 
+    redirectTo: 'ships',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'ships',
+    loadChildren: () => import('./pages/ships/ships.module').then( m => m.ShipsPageModule)
+  },
+  {
+    path: 'ship-details/:id',
+    loadChildren: () => import('./pages/ship-details/ship-details.module').then( m => m.ShipDetailsPageModule)
+  },  
+  {
+    path: 'pilot-details/:id',
+    loadChildren: () => import('./pages/pilot-details/pilot-details.module').then( m => m.PilotDetailsPageModule)
   },
 ];
 
