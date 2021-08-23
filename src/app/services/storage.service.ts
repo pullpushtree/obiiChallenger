7c7f0dfdbe4ce62293b0980d30dacb49b9a50d68
pullpushtree <bad7legend@gmail.com>
2021-08-23 15:14:42 -0400

    Added Storage Service

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  private _storage: Storage | null = null;
+
+  constructor(private storage: Storage) {
+    this.init();
+   }
+
+   async init() {    
+    const storage = await this.storage.create();
+    this._storage = storage;
+   }
+
+   async set(key: string, value: any) {
+    await this._storage?.set(key, value);
+  }
+
+  async get(key: string) {
+    return await this._storage?.get(key);
+  }
+}
